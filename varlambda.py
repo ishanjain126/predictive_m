0e1667cac0945e14d9a03bbba1f524281e45b8e3
ishanjain126 <jain.ishan126@gmail.com>
2020-11-20 14:51:06 +0530

    third commit

--- varlambda.py
+++ varlambda.py
@@ -12,23 +12,26 @@ from sklearn.model_selection import cross_val_score
 from numpy import int64
 
 # #Create a pandas DataFrame for the counts data set.
-# df = pd.read_csv(r'C:\Users\hp\Desktop\bicycle.csv', header=0, infer_datetime_format=True, parse_dates=[0], index_col=[0])
-df = pd.read_csv(r'C:\Users\hp\Documents\predictive modelling\presentation\IRIS.csv')
+df = pd.read_csv(r'C:\Users\hp\Documents\predictive modelling\presentation\bicycle.csv', header=0, infer_datetime_format=True, parse_dates=[0], index_col=[0])
+# df = pd.read_csv(r'C:\Users\hp\Documents\predictive modelling\presentation\IRIS.csv')
 
-# #Set up the X and y matrices
-# y_train, X_train = dmatrices(expr, df_train, return_type='dataframe')
-# y_test, X_test = dmatrices(expr, df_test, return_type='dataframe')
-label_encoder = preprocessing.LabelEncoder()
-df['species'] = label_encoder.fit_transform(df['species'])
-# print(df)
-X = df.loc[:, df.columns != 'species' ]
-y = df['species']
-# y.reshape(-1, 1)
-# X.reshape(-1, 1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 42)
+ds = df.index.to_series()
+df['MONTH'] = ds.dt.month
+df['DAY_OF_WEEK'] = ds.dt.dayofweek
+df['DAY'] = ds.dt.day
+
+#Set up the X and y matrices
+mask = np.random.rand(len(df)) < 0.8
+df_train = df[mask]
+df_test = df[~mask]
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 42)
+expr = """ BB_COUNT ~ DAY + DAY_OF_WEEK + MONTH + HIGH_T + LOW_T + PRECIP """
+y_train, X_train = dmatrices(expr, df_train, return_type = 'dataframe')
+y_test, X_test = dmatrices(expr, df_test, return_type = 'dataframe')
 
 #Using the statsmodels GLM class, train the Poisson regression model on the training data set.
-poisson_training_results = sm.GLM(y_train.astype(float), X_train.astype(float), family=sm.families.Poisson()).fit()
+poisson_training_results = sm.GLM(y_train, X_train, family=sm.families.Poisson()).fit()
 # model = PoissonRegressor(alpha = 1e-12, max_iter = 300).fit(X_train, y_train)
 
 # #Print the training summary.
@@ -41,16 +44,16 @@ print(poisson_training_results.summary())
 # #Make some predictions on the test data set.
 poisson_predictions = poisson_training_results.get_prediction(X_test)
 predictions_summary_frame = poisson_predictions.summary_frame()
-# print(predictions_summary_frame)
+print(predictions_summary_frame)
 
 predicted_counts=predictions_summary_frame['mean']
-actual_counts = y_test
+actual_counts = y_test['BB_COUNT']
 
 # Mlot the predicted counts versus the actual counts for the test data.
 fig = plt.figure()
 fig.suptitle("Actual v/s Predicted")
-predicted = plt.plot(predicted_counts, 'go-', label='Predicted counts')
-actual = plt.plot(actual_counts, 'ro-', label='Actual counts')
+predicted = plt.plot(X_test.index, predicted_counts, 'go-', label='Predicted counts')
+actual = plt.plot(X_test.index, actual_counts, 'ro-', label='Actual counts')
 plt.legend()
 plt.show()
 
